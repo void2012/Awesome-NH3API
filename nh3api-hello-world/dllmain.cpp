ec628cf186c463c494a8a10da1a3c3362151e142
void_17 <61356189+void2012@users.noreply.github.com>
2025-07-08 02:26:11 +0700

    Add dllmain.cpp for the first Hello World

---
+++ nh3api-hello-world/dllmain.cpp
@@ -0,0 +1,28 @@
+#include <nh3api/core.hpp>
+
+void __stdcall HelloWorld(HiHook* hook, void* _this)
+{
+    NormalDialog("Hello, World!");
+    THISCALL_1(void, hook->GetOriginalFunc(), _this);
+}
+
+extern "C" NH3API_DLLEXPORT
+BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+    if ( fdwReason == DLL_PROCESS_ATTACH  )
+    {
+        Patcher* patcher = GetPatcher();
+        if ( patcher == nullptr )
+            return false;
+        PatcherInstance* instance = patcher->CreateInstance("HD.Plugin.NH3API.HelloWorld");
+        if ( instance )
+        {
+            instance->WriteHiHook(0x4EEE31, CALL_, EXTENDED_, THISCALL_, &HelloWorld);
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
